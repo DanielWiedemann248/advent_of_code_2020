f438f86c5fdeff9986a00912b1ae0faf87e85a61
Daniel Wiedemann <daniel.wiedemann@hetzner.com>
2020-12-01 12:43:06 +0100

    day 1: use break instead of continue

--- day1/day1_2.php
+++ day1/day1_2.php
@@ -3,15 +3,15 @@ $puzzle_input =file("puzzle_input.txt", FILE_IGNORE_NEW_LINES);
 for($i = 0; $i < count($puzzle_input); $i++) {
     for ($j = 0; $j < count($puzzle_input); $j++) {
         if($i + 1 + $j >= count($puzzle_input)) {
-            continue;
+            break;
         } else {
             for($k = 0; $k < count($puzzle_input); $k++) {
                 if ($i + 1 + $j + $k >= count($puzzle_input)) {
-                    continue;
+                    break;
                 } else {
                     if ($puzzle_input[$i] + $puzzle_input[$i + 1 + $j] + $puzzle_input[$i + 1 + $j + $k] === 2020) {
                         echo $puzzle_input[$i] . ", " . $puzzle_input[$i + 1 + $j] . " and " . $puzzle_input[$i + 1 + $j + $k] . " equal 2020\n";
-                        echo($puzzle_input[$i] * $puzzle_input[$i + 1 + $j] * $puzzle_input[$i + 1 + $k + $j]);
+                        echo($puzzle_input[$i] * $puzzle_input[$i + 1 + $j] * $puzzle_input[$i + 1 + $k + $j]) . PHP_EOL;
                     }
                 }
             }
